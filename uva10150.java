4d0fdd0e30bc4631e56264513ac67abf0a3071f1
Chao Xu <mgcclx@gmail.com>
2010-02-10 15:16:28 -0500

    doublet wa

--- uva10150.java
+++ uva10150.java
@@ -2,67 +2,99 @@ import java.util.*;
 import java.io.*;
 
 public class uva10150 {
-      public static Set<String> s;
+      public static Map<String,ArrayList<String>> s;
       public static void main(String[] args) throws IOException{
               Scanner in = new Scanner(System.in);
               String str;
               String s1,s2;
-              s = new HashSet<String>();
+              s = new HashMap<String,ArrayList<String>>();
               //construct list of words
+              char[] c;
               while(in.hasNext()){
                       str = in.nextLine();
                       if(str.length()>0){
-                              s.add(str);
+                    	  for(int i =0;i<str.length();i++){
+                    		  c = str.toCharArray();
+                    		  c[i] = '0';
+                    		  String ns = String.valueOf(c);
+                    		  if(!s.containsKey(ns)){
+                    			  s.put(ns, new ArrayList<String>());
+                    		  }
+                    		  if(!s.get(ns).contains(str)){
+                    			  s.get(ns).add(str);
+                    		  }
+                    	  }
                       }else{
                               break;
                       }
               }
               int k=0;
               while(in.hasNext()){
-                  if(k!=0){
-                          System.out.println();
-                  }
                       s1 = in.next();
                       s2 = in.next();
+                      if(k!=0){
+                          System.out.println();
+                      }
+                      boolean no = true;
                       if(s1.length()==s2.length()){
-                    	  if(k<1){
-                    		  search(s1,s2);
+                    	  if(s.containsKey("0".concat(s1.substring(1)))){
+                    		  if(s.get("0".concat(s1.substring(1))).contains(s1)){
+                    			  no = search(s1,s2);
+                    		  }
                     	  }
-                      }else{
-                          System.out.println("No solution.");
+                      }
+                      if(no){
+                    	  System.out.println("No solution.");
                       }
                       k++;
               }
       }
       
-      
-      public static void search(String s1,String s2){
+      public static boolean search(String s1,String s2){
     	  Set<String> done = new HashSet<String>();
+    	  String[] g;
+    	  if(s1.compareTo(s2)==0){
+    		  g = generate_diff(s1);
+    		  for(int i =0;i<g.length;i++){
+    			  if(s.containsKey(g[i])){
+    				  for(int j=0;j<s.get(g[i]).size();j++){
+    					  if(s.get(g[i]).get(j).compareTo(s1)!=0){
+	        				  System.out.println(s1);
+	        				  System.out.println(s.get(g[i]).get(j));
+	        				  System.out.println(s1);
+	        				  return false;
+    					  }
+    				  }
+    			  }
+    		  }
+    	  }
     	  Queue<String[]> next = new LinkedList<String[]>();
     	  Stack<String[]> tested = new Stack<String[]>();
     	  String[] t = new String[2];
     	  t[0] = s1;
     	  String[] r = t.clone();
     	  next.add(t);
-    	  String[] g;
     	  boolean e = false;
     	  while(next.size()>0){
     		  t = next.poll();
-    		  done.add(t[0]);
 			  tested.add(t);
+			  done.add(t[0]);
     		  g = generate_diff(t[0]);
     		  for(int i =0;i<g.length;i++){
-    			  if(g[i]==null){
-    				  break;
-    			  }
-    			  if(s.contains(g[i])&&done.contains(g[i])==false ){
-    				  String[] tmp = new String[2];
-    				  tmp[0] = g[i];
-    				  tmp[1] = t[0];
-    				  next.add(tmp);
-    				  if(g[i].compareTo(s2)==0){
-    					  r = tmp;
-    					  e = true;
+    			  if(s.containsKey(g[i])){
+    				  String[] pos = new String[s.get(g[i]).size()];
+    				  pos = s.get(g[i]).toArray(pos);
+    				  for(int j =0;j<pos.length;j++){
+    					  if(done.contains(pos[j])==false){
+    						  String[] tmp = new String[2];
+    	    				  tmp[0] = pos[j];
+    	    				  tmp[1] = t[0];
+    	    				  next.add(tmp);
+    	    				  if(pos[j].compareTo(s2)==0){
+    	    					  r = tmp;
+    	    					  e = true;
+    	    				  }
+    					  }
     				  }
     			  }
     		  }
@@ -81,58 +113,17 @@ public class uva10150 {
     		  for(int i=out.size()-1;i>-1;i--){
     			  System.out.println(out.get(i));
     		  }
-    	  }else{
-    		  System.out.println("No solution.");
+    		  return false;
     	  }
+    		  return true;
       }
-      
       public static String[] generate_diff(String a){
-    	  String[] r = new String[a.length()*26];
-    	  char[] c = a.toCharArray();
-    	  char[] c2 = c.clone();
-    	  int count = 0;
-    	  for(int i=0;i<a.length();i++){
-    		  for(int j=97;j<123;j++){
-    			  if(c2[i]!= (char) j){
-	    			  c2[i] = (char) j;
-	    			  r[count] = String.valueOf(c2);
-	    			  c2 = c.clone();
-	    			  count++;
-    			  }
-    		  }
+    	  String[] r = new String[a.length()];
+    	  for(int i =0;i<a.length();i++){
+    		  char[] c = a.toCharArray();
+    		  c[i] = '0';
+    		  r[i] = String.valueOf(c);
     	  }
     	  return r;
       }
-      public static boolean diff(String a, String b){
-              int c =0;
-              char[] ac = a.toCharArray();
-              char[] bc = b.toCharArray();
-              for(int i=0;i<a.length();i++){
-                      if(ac[i]!=bc[i]){
-                              c++;
-                              if(c>1){
-                                      return false;
-                              }
-                      }
-              }
-              return true;
-      }
-      
-      public static boolean diff(char[] ac, char[] bc){
-    	  if(ac.length!=bc.length){
-    		  return false;
-    	  }
-    	  int c=0;
-          for(int i=0;i<ac.length;i++){
-              if(ac[i]!=bc[i]){
-                      c++;
-                      if(c>1){
-                              return false;
-                      }
-              }
-      }
-          return true;
-      }
 }
-
-
