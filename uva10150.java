ee5a3efd82fcf4806e5f6ef9940d3fc4a3de68b9
Chao Xu <mgcclx@gmail.com>
2010-02-14 17:48:07 -0500

    AC Doublets

--- uva10150.java
+++ uva10150.java
@@ -1,13 +1,13 @@
 import java.util.*;
-import java.io.*;
 
 public class uva10150 {
-      public static Map<String,ArrayList<String>> s;
-      public static void main(String[] args) throws IOException{
+	
+      public static Map<String,Set<String>> s;
+      public static void main(String[] args){
               Scanner in = new Scanner(System.in);
               String str;
               String s1,s2;
-              s = new HashMap<String,ArrayList<String>>();
+              s = new HashMap<String,Set<String>>();
               //construct list of words
               char[] c;
               while(in.hasNext()){
@@ -18,7 +18,7 @@ public class uva10150 {
                     		  c[i] = '0';
                     		  String ns = String.valueOf(c);
                     		  if(!s.containsKey(ns)){
-                    			  s.put(ns, new ArrayList<String>());
+                    			  s.put(ns, new HashSet<String>());
                     		  }
                     		  if(!s.get(ns).contains(str)){
                     			  s.get(ns).add(str);
@@ -36,86 +36,62 @@ public class uva10150 {
                           System.out.println();
                       }
                       boolean no = true;
-                      if(s1.length()==s2.length()){
-                    	  if(s.containsKey("0".concat(s1.substring(1)))){
-                    		  if(s.get("0".concat(s1.substring(1))).contains(s1)){
-                    			  no = search(s1,s2);
-                    		  }
-                    	  }
-                      }
-                      if(no){
+                      if(s1.length()==s2.length()&&s.containsKey("0".concat(s1.substring(1)))&&s.get("0".concat(s1.substring(1))).contains(s1)){
+                    		search(s1,s2);
+                      }else{
                     	  System.out.println("No solution.");
                       }
                       k++;
               }
       }
       
-      public static boolean search(String s1,String s2){
-    	  Set<String> done = new HashSet<String>();
-    	  String[] g;
-    	  if(s1.compareTo(s2)==0){
-    		  g = generate_diff(s1);
-    		  for(int i =0;i<g.length;i++){
-    			  if(s.containsKey(g[i])){
-    				  for(int j=0;j<s.get(g[i]).size();j++){
-    					  if(s.get(g[i]).get(j).compareTo(s1)!=0){
-	        				  System.out.println(s1);
-	        				  System.out.println(s.get(g[i]).get(j));
-	        				  System.out.println(s1);
-	        				  return false;
-    					  }
-    				  }
-    			  }
-    		  }
+      static void printr(ArrayList<String> s){
+    	  for(int i=0;i<s.size();i++){
+    		  System.out.println(s.get(i));
     	  }
-    	  Queue<String[]> next = new LinkedList<String[]>();
-    	  Stack<String[]> tested = new Stack<String[]>();
-    	  String[] t = new String[2];
-    	  t[0] = s1;
-    	  String[] r = t.clone();
+      }
+      public static void search(String s1, String s2){
+    	  if(s1.compareTo(s2)==0){
+    		  System.out.println(s1);
+    		  return;
+    	  }
+    	  Set<String> visited = new HashSet<String>();
+    	  Set<String> discovered = new HashSet<String>();
+    	  Queue<ArrayList> next = new LinkedList<ArrayList>();
+    	  ArrayList<String> t =new ArrayList<String>();
+    	  String[] g;
+    	  t.add(s1);
+    	  
     	  next.add(t);
-    	  boolean e = false;
+    	  discovered.add(t.get(t.size()-1));
     	  while(next.size()>0){
     		  t = next.poll();
-			  tested.add(t);
-			  done.add(t[0]);
-    		  g = generate_diff(t[0]);
-    		  for(int i =0;i<g.length;i++){
-    			  if(s.containsKey(g[i])){
-    				  String[] pos = new String[s.get(g[i]).size()];
-    				  pos = s.get(g[i]).toArray(pos);
-    				  for(int j =0;j<pos.length;j++){
-    					  if(done.contains(pos[j])==false){
-    						  String[] tmp = new String[2];
-    	    				  tmp[0] = pos[j];
-    	    				  tmp[1] = t[0];
-    	    				  next.add(tmp);
-    	    				  if(pos[j].compareTo(s2)==0){
-    	    					  r = tmp;
-    	    					  e = true;
-    	    				  }
-    					  }
-    				  }
-    			  }
-    		  }
+       		  g = generate_diff(t.get(t.size()-1));
+       		  visited.add(t.get(t.size()-1));
+       		  for(int i=0;i<g.length;i++){
+       			  if(s.containsKey(g[i])){
+       				if(s.get(g[i]).contains(s2)){
+       					t.add(s2);
+       					printr(t);
+       					return;
+       				}else{
+        				  String[] pos = new String[s.get(g[i]).size()];
+        				  pos = s.get(g[i]).toArray(pos);
+    					  for(int j =0;j<pos.length;j++){
+        					  if(visited.contains(pos[j])==false){
+        						  ArrayList<String> t2 = new ArrayList<String>(t);
+        						  t2.add(pos[j]);
+        						  if(!discovered.contains(pos[j])){
+        							  next.add(t2);
+        							  discovered.add(pos[j]);
+        						  }
+        					  }
+        				  }
+       				}
+       			  }
+       		  }
     	  }
-    	  if(e==true){
-    		  ArrayList<String> out = new ArrayList<String>();
-    		  String k = r[1];
-    		  out.add(r[0]);
-    		  while(tested.size()>0){
-    			  r = tested.pop();
-    			  if(r[0].compareTo(k)==0){
-    				  out.add(r[0]);
-    				  k = r[1];
-    			  }
-    		  }
-    		  for(int i=out.size()-1;i>-1;i--){
-    			  System.out.println(out.get(i));
-    		  }
-    		  return false;
-    	  }
-    		  return true;
+    	  System.out.println("No solution.");
       }
       public static String[] generate_diff(String a){
     	  String[] r = new String[a.length()];
