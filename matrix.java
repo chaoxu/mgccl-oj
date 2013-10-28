7fb6bfe3b233674b41522ac95e9ca37a0da0d0f1
mgccl <mgcclx@gmail.com>
2013-10-27 21:02:06 -0500

    some matrix code needed

--- matrix.java
+++ matrix.java
@@ -1,44 +1,68 @@
-
 public class matrix {
-	 private static final double EPSILON = 1e-10;
+	public static double[][] multiply(double[][] A, double[][] B) {
+        int mA = A.length;
+        int nA = A[0].length;
+        int mB = B.length;
+        int nB = A[0].length;
+        if (nA != mB) throw new RuntimeException("Illegal matrix dimensions.");
+        double[][] C = new double[mA][nB];
+        for (int i = 0; i < mA; i++)
+            for (int j = 0; j < nB; j++)
+                for (int k = 0; k < nA; k++)
+                    C[i][j] += A[i][k] * B[k][j];
+        return C;
+    }
+	
+	public static double[][] identity(int n){
+		double[][] z = new double[n][n];
+		for(int i=0;i<n;i++){
+			z[i][i] = 1.0;
+		}
+		return z;
+	}
+	public static double[][] exp(double[][] m, int p){
+		if(p==0) return identity(m.length);
+		if(p==1) return m;
+		return multiply(exp(m,p%2),exp(multiply(m,m),p/2));
+	}
+	
+	private static final double EPSILON = 1e-10;
 
-	    // Gaussian elimination with partial pivoting
-	 public static double[] lsolve(double[][] A, double[] b) {
-	        int N  = b.length;
-	        for (int p = 0; p < N; p++) {
-	            // find pivot row and swap
-	            int max = p;
-	            for (int i = p + 1; i < N; i++) {
-	                if (Math.abs(A[i][p]) > Math.abs(A[max][p])) {
-	                    max = i;
-	                }
-	            }
-	            double[] temp = A[p]; A[p] = A[max]; A[max] = temp;
-	            double   t    = b[p]; b[p] = b[max]; b[max] = t;
-	            // singular or nearly singular
-	            if (Math.abs(A[p][p]) <= EPSILON) {
-	                throw new RuntimeException("Matrix is singular or nearly singular");
-	            }
-	            // pivot within A and b
-	            for (int i = p + 1; i < N; i++) {
-	                double alpha = A[i][p] / A[p][p];
-	                b[i] -= alpha * b[p];
-	                for (int j = p; j < N; j++) {
-	                    A[i][j] -= alpha * A[p][j];
-	                }
-	            }
-	        }
-
-	        // back substitution
-	        double[] x = new double[N];
-	        for (int i = N - 1; i >= 0; i--) {
-	            double sum = 0.0;
-	            for (int j = i + 1; j < N; j++) {
-	                sum += A[i][j] * x[j];
-	            }
-	            x[i] = (b[i] - sum) / A[i][i];
-	        }
-	        return x;
-	 }
-
+    // Gaussian elimination with partial pivoting
+	public static double[] lsolve(double[][] A, double[] b) {
+        int N  = b.length;
+        for (int p = 0; p < N; p++) {
+            // find pivot row and swap
+            int max = p;
+            for (int i = p + 1; i < N; i++) {
+                if (Math.abs(A[i][p]) > Math.abs(A[max][p])) {
+                    max = i;
+                }
+            }
+            double[] temp = A[p]; A[p] = A[max]; A[max] = temp;
+            double   t    = b[p]; b[p] = b[max]; b[max] = t;
+            // singular or nearly singular
+            if (Math.abs(A[p][p]) <= EPSILON) {
+                throw new RuntimeException("Matrix is singular or nearly singular");
+            }
+            // pivot within A and b
+            for (int i = p + 1; i < N; i++) {
+                double alpha = A[i][p] / A[p][p];
+                b[i] -= alpha * b[p];
+                for (int j = p; j < N; j++) {
+                    A[i][j] -= alpha * A[p][j];
+                }
+            }
+        }
+        // back substitution
+        double[] x = new double[N];
+        for (int i = N - 1; i >= 0; i--) {
+            double sum = 0.0;
+            for (int j = i + 1; j < N; j++) {
+                sum += A[i][j] * x[j];
+            }
+            x[i] = (b[i] - sum) / A[i][i];
+        }
+        return x;
+ 	}
 }
